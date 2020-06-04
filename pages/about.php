dd8907398bc8a21427088a88ad881a47b2cbb384
Bryan-CF2M <web2020.bryan@gmail.com>
2020-06-04 15:12:48 +0200

    début page about

--- pages/about.php
+++ pages/about.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pretty-checkbox@3.0/dist/pretty-checkbox.min.css">
-    <title>Contact</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pretty-checkbox@3.0/dist/pretty-checkbox.min.css">
+    <title>A Propos</title>
   </head>
   <body>
   <?php 
@@ -18,7 +18,21 @@
   require('pages/header.php');
   
   ?>
-
+  <section class="container page-section">
+      <div class="row pb-5">
+          <div class="col-6 align-self-center">
+            <p class="bs-2 pb-2">BRYAN SCHUTTERS </p>
+            <p>Développeur web</p>
+            <br>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim excepturi reiciendis dolore. Obcaecati incidunt labore voluptas, recusandae soluta ipsam numquam officiis laboriosam eaque aliquid mollitia doloremque, quas, blanditiis natus doloribus. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia, esse facilis! A amet aut, nihil, quidem rem expedita iusto id facere omnis veritatis harum voluptas excepturi! Quia molestiae autem ipsa.</p>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-6 d-flex justify-content-start">
+        <ion-icon name="logo-html5"></ion-icon>
+        </div>
+      </div>
+</section>
 
 
 
@@ -29,6 +43,7 @@
   ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
